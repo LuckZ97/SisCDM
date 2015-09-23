ecb9d644f4cefffeb66c632a7025c21b485e9e22
LuckZ97 <ernest-zg-te@hotmail.com>
2015-09-23 05:18:09 -0600

    Proyecto Creado

---
+++ SisCDM/src/siscdm/SisCDM.java
@@ -0,0 +1,32 @@
+
+package siscdm;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class SisCDM  extends JFrame{
+    SisCDM(){
+        
+        this.setBounds(0, 0, 400, 300);
+        this.setVisible(true);
+        
+        
+        String user;
+        String password;
+        
+        JLabel lblUser = new JLabel("Ingrese su Usuario");
+        lblUser.setBounds(20, 10, 30, 100);
+        this.add(lblUser);
+        
+        JTextField txtfUser = new JTextField();
+        txtfUser.setBounds(50, 20, 30, 100);
+        JLabel lblPass = new JLabel("Ingrese su Contraseña");
+        JTextField txtfPass = new JTextField();
+        
+    }
+   
+    public static void main(String[] args) {
+        new SisCDM();
+    }
+    
+}
